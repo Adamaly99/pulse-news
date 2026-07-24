66b6e6114fabc3a532ed9c7006bf41b9e18bc2b7
Adamaly99 <adamsly990@gmail.com>
2026-07-24 18:57:10 +0000

    Remove sitemap integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pulse-news.vercel.app',
-  integrations: [sitemap()],
 });
